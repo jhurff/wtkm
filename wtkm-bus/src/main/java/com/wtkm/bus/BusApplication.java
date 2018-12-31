82f0dbc406d0ecff12ac5d8f09576378921b26c0
Jimmy Hurff <jhurff@gmail.com>
2018-12-31 07:57:05 -0500

    Adding bus

---
+++ wtkm-bus/src/main/java/com/wtkm/bus/BusApplication.java
@@ -0,0 +1,21 @@
+package com.wtkm.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class BusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello WTKM bus!";
+	}
+
+}
+
